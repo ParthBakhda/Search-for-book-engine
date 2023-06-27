68244f786f9008599a889f8ac09f0af52a09c00c
Parth Bakhda <bakhdaparth@gmail.com                                                                                                                                                             git config --global user.name Parth>
2023-06-26 23:38:06 -0700

    Second Push

--- solid-broccoli-main/Develop/client/src/App.js
+++ solid-broccoli-main/Develop/client/src/App.js
@@ -1,3 +1,47 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./path/to/your/schema-and-resolvers');
+
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+const PORT = 4000; // Replace with your desired port number
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/graphql`);
+});
+
+
+
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs } = require('./path/to/your/schema');
+const { resolvers } = require('./path/to/your/resolvers');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const express = require('express');
+const app = express();
+
+server.applyMiddleware({ app });
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+
+
+
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
